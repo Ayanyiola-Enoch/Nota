9a13180d51c4e3d844a7092ddf456664ec2d1164
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2024-12-02 15:03:21 +0100

    Started new notes

---
+++ app/_layout.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import Home from '../src/Screens/Home'
+
+const _layout = () => {
+  return (
+    <View>
+      <Home />
+    </View>
+  )
+}
+
+export default _layout
+
+const styles = StyleSheet.create({})
