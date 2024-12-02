851360aedacb84f36b10394dbb6f851b9dc08ae6
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2024-12-02 15:35:28 +0100

    FIxed status bar and padding

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,15 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import { FONTS, COLORS } from '../constants';
+import { FONTS, COLORS, SIZES } from '../constants';
+import { StatusBar } from "expo-status-bar";
 
 const Home = () => {
     return (
-        <View>
-            <Text style={{ ...FONTS, color: COLORS.white }}>Home</Text>
+        <View style={styles.container}>
+            <StatusBar backgroundColor='grey' style='light' />
+            <View>
+                <Text style={{ ...FONTS, color: COLORS.black }}>Home</Text>
+            </View>
         </View>
     )
-}
+};
 
 export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.chocolateBackground,
+        padding: SIZES.height
+    }
+});
