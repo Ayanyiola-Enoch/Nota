bafb9d3f6d75b8ee8d90230fb2ecb07bde910902
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2024-12-03 12:07:09 +0100

    Continue today

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -10,6 +10,13 @@ const Home = () => {
             <View>
                 <Text style={{ ...FONTS.h4, color: COLORS.black, textAlign: 'center' }}>Nota</Text>
             </View>
+
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                <TouchableOpacity onPress={() => clearTodo()}>
+                    <Image source={icons.deleteImg} style={{ width: SIZES.h2, height: SIZES.h2 }} />
+                </TouchableOpacity>
+            </View>
+
         </View >
     )
 };
