8ee96fd2dd3ed660f649c35cd75f51ff7ad69d9d
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2024-12-03 14:28:19 +0100

    fixed an error, renamed screens

--- app/_layout.js
+++ app/_layout.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react';
-import Home from '../src/Screens/Home'
+import Home from '../src/screens/Home'
 import { COLORS, SIZES } from '../src/constants'
 
 const _layout = () => {
