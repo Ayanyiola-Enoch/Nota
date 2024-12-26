fe4a139915838c1a9ef4f0e7ee812a40d51f34d8
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2024-12-26 17:01:05 +0100

    Adjusted the toast

--- app/_layout.js
+++ app/_layout.js
@@ -2,10 +2,12 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react';
 import Home from '../src/screens/Home'
 import { COLORS, SIZES } from '../src/constants'
+import Toast from 'react-native-toast-message';
 
 const _layout = () => {
   return (
     <View style={styles.container}>
+      <Toast />
       <Home />
     </View>
   )
